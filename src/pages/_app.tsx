4e2551e68b145169bf3ba95457544bc49feaa94d
Ribbit <echo07200503@gmail.com>
2022-05-16 14:23:43 +0900

    ✨ Googleタグマネージャーを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
+import { Suspense } from 'react';
 import type { AppProps } from 'next/app';
 import 'src/styles/reset.css';
 import 'src/styles/globals.css';
 
 import Layout from 'src/components/layout';
+import { GoogleTagManager } from 'src/components/functional/google-tag-manager';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
+const App = ({ Component, pageProps }: AppProps) => (
+  <Suspense fallback={<p>読み込み中...</p>}>
+    <GoogleTagManager />
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
-}
+  </Suspense>
+);
 
-export default MyApp;
+export default App;
