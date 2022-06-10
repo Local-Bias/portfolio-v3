0872870241fdac92297f6e9d6d5a11c62db4b37f
Ribbit <echo07200503@gmail.com>
2022-06-10 10:42:42 +0900

    ✨ データページにISRを使ってkintoneプラグインの実績を表示

--- src/components/pages/data/index.tsx
+++ src/components/pages/data/index.tsx
@@ -1,8 +1,39 @@
 import Head from 'next/head';
 import React, { FC } from 'react';
+import { useIndicator } from 'src/components/providers/indicator';
 import { meta } from 'src/static/site-config';
 
 const TITLE = `数字で知るRibbit`;
+
+const UserData: FC = () => {
+  const { indicator, lastModified } = useIndicator();
+
+  if (!indicator) {
+    return <div>利用者情報の取得に失敗しました</div>;
+  }
+
+  return (
+    <section>
+      <h2>kintoneプラグインの利用者情報</h2>
+      <p>オープンソースで公開しているkintoneプラグインの利用状況です。</p>
+      <section>
+        <h3>利用法人数</h3>
+        <p>
+          現在{indicator.numUsers.toLocaleString()}社にご利用いただいています。(最終更新:{' '}
+          {lastModified})
+        </p>
+        <img src='/img/products/number-of-users.webp' />
+      </section>
+      <section>
+        <h3>実行回数</h3>
+        <p>
+          プラグインがCDN経由で{indicator.counter.toLocaleString()}回呼び出されました。(最終更新:{' '}
+          {lastModified})
+        </p>
+      </section>
+    </section>
+  );
+};
 
 const Component: FC = () => (
   <>
@@ -17,9 +48,10 @@ const Component: FC = () => (
       </div>
       <h1 className='mt-8'>{TITLE}</h1>
       <section></section>
+      <UserData />
       <section>
         <h2>Googleデータポータル</h2>
-        <p>Google Analyticsを使って収集した、</p>
+        <p>Google Analyticsを使って収集した、私が開発した各サイトの利用状況です。</p>
         <iframe
           width='768'
           height='768'
@@ -31,5 +63,9 @@ const Component: FC = () => (
     </article>
   </>
 );
+
+const Container: FC = () => {
+  return <Component />;
+};
 
 export default Component;
--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -1,6 +1,26 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
+import { IndicatorProvider } from 'src/components/providers/indicator';
+import { fetchKintonePluginIndicator } from 'src/lib/external';
 import Page from 'src/components/pages/data';
+import { getFormattedDate } from 'src/lib/util';
 
-const DataRoot: NextPage = () => <Page />;
+type StaticProps = { indicator: external.Indicator | null; lastModified: string };
 
-export default DataRoot;
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
+  const indicator = await fetchKintonePluginIndicator();
+
+  const lastModified = getFormattedDate(new Date(), 'M月d日 h時m分');
+
+  return {
+    props: { indicator, lastModified },
+    revalidate: 10 * 60,
+  };
+};
+
+const DashboardRoot: NextPage<StaticProps> = ({ indicator, lastModified }) => (
+  <IndicatorProvider initialValue={{ indicator, lastModified }}>
+    <Page />
+  </IndicatorProvider>
+);
+
+export default DashboardRoot;
