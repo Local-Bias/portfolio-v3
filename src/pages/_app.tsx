8e696d09db7f6c629132e99c052c180a7165d400
Ribbit <echo07200503@gmail.com>
2022-06-21 17:32:54 +0900

    👍 transport gtm component from _document to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,11 @@ import 'src/styles/reset.css';
 import 'src/styles/globals.css';
 
 import Layout from 'src/components/layout';
+import { GoogleTagManager } from 'src/components/functional/google-tag-manager';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Suspense fallback={<p>読み込み中...</p>}>
+    <GoogleTagManager />
     <Layout>
       <Component {...pageProps} />
     </Layout>
